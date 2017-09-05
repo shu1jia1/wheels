801d5ce2d352f4a36671edfe4e7cb9fac36502df
Shu jia <shu1jia1@gmail.com>
2017-09-06 00:21:49 +0800

    heart beat handler

--- st-site/src/main/java/com/st/modules/heartbeat/HeartBeatEventHandler.java
+++ st-site/src/main/java/com/st/modules/heartbeat/HeartBeatEventHandler.java
@@ -1,18 +1,26 @@
 package com.st.modules.heartbeat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.eventbus.Subscribe;
+import com.st.common.message.STProtoMessage;
+import com.st.common.message.entity.STCommon.Address;
 import com.st.common.message.entity.StMessage.LoginRequet;
+import com.st.modules.message.event.HeartbeatReceiveEvent;
 
 public class HeartBeatEventHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(HeartBeatEventHandler.class);
+    
     @Subscribe
-    public void handlerHeartBeat(String deviceNo) {
-
+    public void handlerHeartBeat(HeartbeatReceiveEvent heartEvent) {
+        STProtoMessage msg = heartEvent.getStMessage();
+        Address src = msg.getSrc();
+        logger.info("recevie heart beat from {}", src.toString());
     }
 
     @Subscribe
     public void handlerLogin(LoginRequet request) {
-        
 
     }
 
