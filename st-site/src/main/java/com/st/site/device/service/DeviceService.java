db488943e6951ac64f7e6b6bd4afc9bc9fc268b4
Shu jia <shu1jia1@gmail.com>
2017-09-06 00:23:48 +0800

    login handler

--- st-site/src/main/java/com/st/site/device/service/DeviceService.java
+++ st-site/src/main/java/com/st/site/device/service/DeviceService.java
@@ -1,7 +1,10 @@
 package com.st.site.device.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +12,13 @@ import org.springframework.stereotype.Service;
 import com.github.shu1jia1.common.utils.string.PrintUtil;
 import com.github.shu1jia1.site.base.entity.ResponseData;
 import com.github.shu1jia1.site.base.entity.builder.ResponseBuilder;
+import com.st.common.message.entity.StMessage.LoginRequet;
 import com.st.site.device.dao.DeviceDao;
 import com.st.site.device.entity.DeviceInfo;
 
 @Service("deviceService")
 public class DeviceService implements IDeviceService, InitializingBean {
+    private static final Logger logger = LoggerFactory.getLogger(DeviceService.class);
 
     @Autowired
     private DeviceDao deviceDao;
@@ -27,6 +32,23 @@ public class DeviceService implements IDeviceService, InitializingBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println(PrintUtil.toString(deviceDao.listDevice("geo1")));
 
+    }
+
+    public boolean deviceLogIn(LoginRequet innerPayload, String ip) {
+        String devno = innerPayload.getDevno();
+        String version = innerPayload.getVersion();
+        int type = innerPayload.getType();
+        DeviceInfo device = new DeviceInfo(devno, type, version);
+        device.setIp(ip);
+        device.setUpdateTime(new Timestamp(System.currentTimeMillis()));
+        device.setName(devno);
+        try {
+            deviceDao.insertOrUpdate(device);
+            return true;
+        } catch (Exception e) {
+            logger.info("device {} login failed", device);
+            return false;
+        }
     }
 
 }
